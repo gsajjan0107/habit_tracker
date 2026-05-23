f933b2009292bc7e25ea1ad2c9a6699f5e6c058e
gsajjan <gsajjan0107@gmail.com>
2026-05-23 11:17:23 +0530

    Add tests for active habit date helper

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,4 +1,4 @@
-from helpers import get_confirmation
+from helpers import get_confirmation, is_habit_active_on_date
 
 
 def test_get_confirmation_yes(monkeypatch):
@@ -18,3 +18,48 @@ def test_get_confirmation_invalid_then_yes(monkeypatch):
     monkeypatch.setattr("builtins.input", lambda _: next(answers))
 
     assert get_confirmation("Confirm? ") is True
+
+
+def test_is_habit_active_on_date_before_created_date():
+    info = {
+        "created_at": "2026-05-10",
+        "archived_at": None
+    }
+
+    assert is_habit_active_on_date(info, "2026-05-09") is False
+
+
+def test_is_habit_active_on_date_on_created_date():
+    info = {
+        "created_at": "2026-05-10",
+        "archived_at": None
+    }
+
+    assert is_habit_active_on_date(info, "2026-05-10") is True
+
+
+def test_is_habit_active_on_date_between_created_and_archived_date():
+    info = {
+        "created_at": "2026-05-10",
+        "archived_at": "2026-05-15"
+    }
+
+    assert is_habit_active_on_date(info, "2026-05-12") is True
+
+
+def test_is_habit_active_on_date_on_archived_date():
+    info = {
+        "created_at": "2026-05-10",
+        "archived_at": "2026-05-15"
+    }
+
+    assert is_habit_active_on_date(info, "2026-05-15") is True
+
+
+def test_is_habit_active_on_date_after_archived_date():
+    info = {
+        "created_at": "2026-05-10",
+        "archived_at": "2026-05-15"
+    }
+
+    assert is_habit_active_on_date(info, "2026-05-16") is False
